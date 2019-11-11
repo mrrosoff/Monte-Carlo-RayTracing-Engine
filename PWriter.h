5cdff533f76d4b6376970d0047e4e066ed546b8e
Max Rosoff <45046027+mrrosoff@users.noreply.github.com>
2019-11-11 14:10:05 -0700

    Add files via upload

---
+++ PWriter.h
@@ -0,0 +1,35 @@
+//
+// Created by Max Rosoff on 9/21/2019.
+//
+
+#ifndef GRAPHICS_PWRITER_H
+#define GRAPHICS_PWRITER_H
+
+#include <string>
+#include <vector>
+
+#include <fstream>
+
+#include <cerrno>
+#include <cstring>
+
+#include "../Eigen/Eigen/Eigen"
+
+class PWriter {
+
+public:
+
+    explicit PWriter(const std::string &);
+    PWriter &operator<<(const std::vector<std::vector<std::vector<int>>> &);
+
+    PWriter(const PWriter &) = delete;
+    PWriter &operator=(const PWriter &) = delete;
+    ~PWriter() = default;
+
+private:
+
+    std::string outFile;
+};
+
+
+#endif //GRAPHICS_PWRITER_H
